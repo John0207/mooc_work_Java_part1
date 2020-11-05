c414779c184df8b646ac58bbfee68673ebd4ee86
John0207 <john.deluccia@snhu.edu>
2020-11-05 02:01:54 -0500

    story

--- src/main.java
+++ src/main.java
@@ -4,10 +4,14 @@ public class main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Greetings! How are you doing?");
-        String first = scanner.nextLine();
-        System.out.println("Oh, how interesting. Tell me more!");
-        String second = scanner.nextLine();
-        System.out.println("Thanks for sharing!");
+        System.out.println("I will tell you a story, but I need some information first.");
+        System.out.println("What is the main character called?");
+        String name = scanner.nextLine();
+        System.out.println("What is their job?");
+        String job = scanner.nextLine();
+        System.out.println("Here is the story: ");
+        System.out.println("Once upon a time there was " + name + " , who was a " + job + ".");
+        System.out.println("On the way to work, " + name + " reflected on life.");
+        System.out.println("Perhaps " + name + " will not be a " + job + " forever.");
     }
 }
